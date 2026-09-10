e4443de51c2daac087fa2c863d1fea234f366d52
Kartavya728 <kartavya.xenon@gmail.com>
2026-09-10 15:45:36 +0530

    hh

--- data/grid_data/index.ts
+++ data/grid_data/index.ts
@@ -42,7 +42,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently part of Exodia'25 Web-dev team",
+    title: "Got an internship at Microsoft for upcoming summer 🥳",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
